d215727dcbeb189877452c473a6e95ccc0a70732
jotrorox <mail@jotrorox.com>
2026-04-10 22:41:49 +0200

    Moving the startup section into the library code and simplify the flashing process

--- examples/blink/scripts/flash.sh
+++ examples/blink/scripts/flash.sh
@@ -3,7 +3,5 @@ set -e
 
 c3c build
 
-avr-objcopy -O ihex -R .eeprom build/blink.elf blink.hex
-
 avrdude -c arduino -p atmega328p -P /dev/ttyACM0 -b 115200 \
-    -U flash:w:blink.hex
+    -U flash:w:build/blink.elf:e
